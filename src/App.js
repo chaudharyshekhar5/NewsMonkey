9204f80c05bb3928d125c44c9331000eaa9ece7a
chaudharyshekhar5 <80560965+chaudharyshekhar5@users.noreply.github.com>
2023-02-15 22:29:23 +0530

    readme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/NewsGeneral";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import NewsBusiness from "./components/NewsBusiness";
 import NewsTechnology from "./components/NewsTechnology";
 import NewsSports from "./components/NewsSports";
@@ -18,20 +23,19 @@ export default class App extends Component {
           <Routes>
             <Route path="/" element={<News key="general" />} />
             <Route path="/sports" element={<NewsSports key="sports" />} />
-
             <Route path="/science" element={<NewsScience key="science" />} />
-
             <Route
               path="/entertainment"
               element={<NewsEntertainment key="entertainment" />}
             />
-
             <Route
               path="/technology"
               element={<NewsTechnology key="technology" />}
             />
             <Route path="/business" element={<NewsBusiness key="business" />} />
             <Route path="/health" element={<NewsHealth key="health" />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+            const name = new type(arguments);
           </Routes>
         </Router>
       </div>
