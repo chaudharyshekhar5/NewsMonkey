e27a6810464aa9fae5fb18c36d2256cd79190506
chaudharyshekhar5 <80560965+chaudharyshekhar5@users.noreply.github.com>
2023-02-15 07:17:55 +0530

    first commit

---
+++ src/components/News.jsx
@@ -0,0 +1,121 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import propTypes from "prop-types";
+
+export class News extends Component {
+  static defaultProps = {
+    country: "in",
+    pageSize: 6,
+    category: "general",
+  };
+  static propTypes = {
+    country: propTypes.string,
+    pageSize: propTypes.number,
+    category: propTypes.string,
+  };
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
+
+  async handleUpdate() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=0ca453bc7e1e4871817bc9bdf6825677&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      loading: false,
+      totalResults: parsedData.totalResults,
+    });
+  }
+  async componentDidMount() {
+    this.handleUpdate();
+  }
+  handlePrevClick = async () => {
+    this.setState({
+      page: this.state.page - 1,
+    });
+    this.handleUpdate();
+  };
+
+  handleNextClick = async () => {
+    this.setState({
+      page: this.state.page + 1,
+    });
+    this.handleUpdate();
+  };
+
+  render() {
+    return (
+      <div className="container my-2">
+        <h1
+          className="text-center"
+          style={{
+            color: "red",
+          }}
+        >
+          YOU GET DAILY NEWS HERE{" "}
+        </h1>
+        {this.state.loading && <Spinner />}
+        <div className="row">
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={
+                      element.title ? element.title.slice(0, 45) : "Latest News"
+                    }
+                    description={
+                      element.description
+                        ? element.description.slice(0, 88)
+                        : "For more detail click on Read More"
+                    }
+                    newsUrl={element.url}
+                    imageUrl={
+                      element.urlToImage
+                        ? element.urlToImage
+                        : "https://content.fortune.com/wp-content/uploads/2023/02/GettyImages-1261552395-e1676242511548.jpg?resize=1200,600"
+                    }
+                    date={
+                      element.publishedAt ? element.publishedAt : "2 days ago"
+                    }
+                    author={element.author ? element.author : "Unknown"}
+                  />
+                </div>
+              );
+            })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-success"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            type="button"
+            className="btn btn-success"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
